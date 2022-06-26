a7d30f367610f229fbaa40c2cf8097045680800b
Rafael Uen <rafaeluen@gmail.com>
2022-06-26 15:39:59 -0300

    teste para Shad

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -48,6 +48,7 @@ export default function PokemonCard({
         <div className='pokemon-name-text'>{`#${pokemonData.id} ${pokemon.name}`}</div>
       </div>
       <div className='pokemon-info'>
+        <div>12345</div>
         <div className='pokemon-image'>
           <img src={pokemonData?.sprites?.front_default} width='150' />
         </div>
